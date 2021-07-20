6911bafff1fe583ed5136fe52c53b6a3868cb9a7
Dylan Shoemaker <me@dylanshoemaker.com>
2021-07-19 18:37:23 -0600

    change naming convention

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ $("#currentDay").text(moment().format("dddd, MMMM Do ")); //https://momentjscom.
 
 //store the data https://stackoverflow.com/questions/61583553/how-to-use-jquery-to-save-the-text-on-the-page-even-after-refreshing-it
 $(".saveBtn").click(function () {
+  console.log($(this));
+  console.log($(".saveBtn"));
   var time = $(this).parent().attr("id");
   var textarea = $(this).siblings(".textarea").val();
   localStorage.setItem(time, textarea);
@@ -11,10 +13,11 @@ $(".saveBtn").click(function () {
 
 //retrieve the data
 $(".time-block").each(function () {
+  console.log($(".time-block"));
   var honeyBunchesOfOats = $(this).attr("id");
-  var textarea = localStorage.getItem(honeyBunchesOfOats);
-  if (textarea !== null) {
-    $(this).children(".textarea").val(textarea);
+  var keyEl = localStorage.getItem(honeyBunchesOfOats);
+  if (keyEl !== null) {
+    $(this).children(".textarea").val(keyEl);
   }
 });
 
@@ -23,7 +26,7 @@ function urgencyNotifier() {
   hour = time.hours();
   $(".time-block").each(function() {
       var currentHour = parseInt($(this).attr("id"));
-
+      
       if (currentHour > hour) {
         $(this).removeClass(["past", "present"]).addClass("future");
       } 
